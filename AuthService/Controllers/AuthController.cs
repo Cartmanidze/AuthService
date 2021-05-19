a3be631db9b18e2605d32a7ffea7108a2efafa40
Глеб Безруков <bezrukov96@outlook.com>
2021-05-19 07:03:52 +0300

    Добавьте файлы проекта.

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using AuthService.Models;
+using AuthService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp(SignUpModel signUpModel)
+        {
+            return await _authService.SignUp(signUpModel);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            return await _authService.Login(loginModel);
+        }
+    }
+}
---
+++ AuthService/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using AuthService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Services
+{
+    public interface  IAuthService
+    {
+        Task<ObjectResult> SignUp(SignUpModel signUpModel);
+        Task<ObjectResult> Login(LoginModel loginModel);
+    }
+}
